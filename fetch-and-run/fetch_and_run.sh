8712805ee78fd861b47b5d3dcbba5848bfa63cd1
Dan Tenenbaum <dtenenba@fhcrc.org>
2018-06-08 11:28:41 -0700

    first cut at generalized fetch and run

--- fetch-and-run/fetch_and_run.sh
+++ fetch-and-run/fetch_and_run.sh
@@ -27,13 +27,13 @@ usage () {
 Usage:
 
 export BATCH_FILE_TYPE="script"
-export BATCH_FILE_S3_URL="s3://my-bucket/my-script"
+export BATCH_FILE_URL="s3://my-bucket/my-script"
 ${BASENAME} script-from-s3 [ <script arguments> ]
 
   - or -
 
 export BATCH_FILE_TYPE="zip"
-export BATCH_FILE_S3_URL="s3://my-bucket/my-zip"
+export BATCH_FILE_URL="s3://my-bucket/my-zip"
 ${BASENAME} script-from-zip [ <script arguments> ]
 ENDUSAGE
 
@@ -48,20 +48,21 @@ error_exit () {
 
 # Check what environment variables are set
 if [ -z "${BATCH_FILE_TYPE}" ]; then
-  usage "BATCH_FILE_TYPE not set, unable to determine type (zip/script) of URL ${BATCH_FILE_S3_URL}"
+  usage "BATCH_FILE_TYPE not set, unable to determine type (zip/script) of URL ${BATCH_FILE_URL}"
 fi
 
-if [ -z "${BATCH_FILE_S3_URL}" ]; then
-  usage "BATCH_FILE_S3_URL not set. No object to download."
+if [ -z "${BATCH_FILE_URL}" ]; then
+  usage "BATCH_FILE_URL not set. No object to download."
 fi
 
-scheme="$(echo "${BATCH_FILE_S3_URL}" | cut -d: -f1)"
-if [ "${scheme}" != "s3" ]; then
-  usage "BATCH_FILE_S3_URL must be for an S3 object; expecting URL starting with s3://"
-fi
+# scheme="$(echo "${BATCH_FILE_URL}" | cut -d: -f1)"
+# if [ "${scheme}" != "s3" ]; then
+#   usage "BATCH_FILE_URL must be for an S3 object; expecting URL starting with s3://"
+# fi
 
 # Check that necessary programs are available
 which aws >/dev/null 2>&1 || error_exit "Unable to find AWS CLI executable."
+which curl >/dev/null 2>&1 || error_exit "Unable to find curl executable."
 which unzip >/dev/null 2>&1 || error_exit "Unable to find unzip executable."
 
 # Create a temporary directory to hold the downloaded contents, and make sure
@@ -83,7 +84,7 @@ install -m 0600 /dev/null "${TMPFILE}" || error_exit "Failed to create temp file
 # Fetch and run a script
 fetch_and_run_script () {
   # Create a temporary file and download the script
-  aws s3 cp "${BATCH_FILE_S3_URL}" - > "${TMPFILE}" || error_exit "Failed to download S3 script."
+  curl -L "${BATCH_FILE_URL}" > "${TMPFILE}" || error_exit "Failed to download script"
 
   # Make the temporary file executable and run it with any given arguments
   local script="./${1}"; shift
@@ -94,7 +95,8 @@ fetch_and_run_script () {
 # Download a zip and run a specified script from inside
 fetch_and_run_zip () {
   # Create a temporary file and download the zip file
-  aws s3 cp "${BATCH_FILE_S3_URL}" - > "${TMPFILE}" || error_exit "Failed to download S3 zip file from ${BATCH_FILE_S3_URL}"
+
+  curl -L "${BATCH_FILE_URL}" > "${TMPFILE}" || error_exit "Failed to download S3 zip file from ${BATCH_FILE_URL}"
 
   # Create a temporary directory and unpack the zip file
   cd "${TMPDIR}" || error_exit "Unable to cd to temporary directory."
@@ -102,7 +104,7 @@ fetch_and_run_zip () {
 
   # Use first argument as script name and pass the rest to the script
   local script="./${1}"; shift
-  [ -r "${script}" ] || error_exit "Did not find specified script '${script}' in zip from ${BATCH_FILE_S3_URL}"
+  [ -r "${script}" ] || error_exit "Did not find specified script '${script}' in zip from ${BATCH_FILE_URL}"
   chmod u+x "${script}" || error_exit "Failed to chmod script."
   exec "${script}" "${@}" || error_exit " Failed to execute script."
 }
